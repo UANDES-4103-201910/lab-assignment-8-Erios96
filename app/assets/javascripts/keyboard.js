5232c650d4f0ad76607eab0a75c94a2e283d5bc5
Ernesto Ríos <ernestorios@MacBook-Pro-de-Ernesto.local>
2019-05-09 22:46:26 -0400

    lab 8

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -2,3 +2,17 @@
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
 });
+
+var hi = "";
+$(document).ready(function(){
+    $("div.key").click(function(){
+        hi += $(this).text()
+        $("#text_space").text(hi);
+    });
+});
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("div.key").toggle();
+    });
+});
